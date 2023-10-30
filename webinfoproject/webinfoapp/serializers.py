e7ea8c5a772d84e16c850098010fc247a5c3fd1d
Vineeth-davis <102505719+Vineeth-davis@users.noreply.github.com>
2023-10-30 19:03:02 +0530

    Update serializers.py

--- webinfoproject/webinfoapp/serializers.py
+++ webinfoproject/webinfoapp/serializers.py
@@ -3,10 +3,14 @@ from rest_framework import serializers
 from .models import Platform, Product,Device
 
 class PlatformSerializer(serializers.ModelSerializer):
+    modified_by = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Platform
         fields = '__all__'
+
+
 class ProductSerializer(serializers.ModelSerializer):
+    modified_by = serializers.StringRelatedField(read_only=True)
     class Meta:
         model = Product
         fields = '__all__'
@@ -24,6 +28,7 @@ class DeviceSerializer(serializers.ModelSerializer):
 
 '''
 
+'''
 class DeviceSerializer(serializers.ModelSerializer):
     class Meta:
         model = Device
@@ -35,4 +40,51 @@ class DeviceSerializer(serializers.ModelSerializer):
         if not user.is_superuser and user.is_staff:
             data['platform'] = instance.platform.name if instance.platform else None
             data['product'] = instance.product.name if instance.product else None
+        return data
+    
+'''
+class DeviceSerializer(serializers.ModelSerializer):
+    modified_by = serializers.StringRelatedField(read_only=True)
+    class Meta:
+        model = Device
+        fields = '__all__'
+
+    def to_representation(self, instance):
+        data = super().to_representation(instance)
+        user = self.context['request'].user
+        if not user.is_superuser and user.is_staff:
+            # If the user is a technician, include the platform and product data
+
+            data['product'] = {
+                'id': instance.product.id,
+                'platform': {
+                    'id': instance.platform.id,
+                    'name': instance.platform.name
+                },
+                'name': instance.product.name,
+                'created_at': instance.product.created_at,
+                'updated_at': instance.product.updated_at,
+                'modified_by': instance.product.modified_by
+            }
+            data['platform'] = {
+                'id': instance.platform.id,
+                'name': instance.platform.name
+            }
+        if user.is_superuser:
+
+            data['product'] = {
+                'id': instance.product.id,
+                'platform': {
+                    'id': instance.platform.id,
+                    'name': instance.platform.name
+                },
+                'name': instance.product.name,
+                'created_at': instance.product.created_at,
+                'updated_at': instance.product.updated_at,
+                'modified_by': instance.product.modified_by
+            }
+            data['platform'] = {
+                'id': instance.platform.id,
+                'name': instance.platform.name
+            }
         return data
